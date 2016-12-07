9afd09dc13102e16880bed14266bfea905e20d56
iamstd <iamstd@163.com>
2016-12-07 13:49:19 +0800

    add three.js

---
+++ ES2015/chap-three.js
@@ -0,0 +1,11 @@
+// for-in 与for-of 的遍历方式
+var obj = {name: 'lee', age: '27', sex: 'male'};
+var array = [1, 2, 3, 4, 3, 2, 1];
+var str = 'iamstd';
+
+ // for-in 只能循环带有iteral属性的数据结构
+for (var i in obj) {
+    console.log(i) //  name, age, sex
+    
+    // shiyong
+}  
